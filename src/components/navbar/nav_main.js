d154c0772c95c5cc0a7ade5346521c51a0dbd274
bkim <w1057216@g.ucla.edu>
2020-10-17 00:56:37 -0700

    Apply scrollspy to navbar

--- src/components/navbar/nav_main.js
+++ src/components/navbar/nav_main.js
@@ -1,31 +1,82 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {Link, animateScroll as scroll} from 'react-scroll';
 
-const NavbarMain = (props) => {
+class NavbarMain extends Component {
 
-    return (
-        <React.Fragment>
-            <Navbar bg="light" expand="lg" fixed="top" role="navigation">
-                <Navbar.Brand href="#landing">
-                    <div className="text-bento-primary font-primary border border-3 border-bento-primary p-1 ml-2">
-                        bdhk
-                    </div>
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="menu" />
-                <Navbar.Collapse id="menu" className="justify-content-end mr-5">
-                    <Nav className="font-secondary">
-                        <Nav.Item>
-                            <Nav.Link href="#about">About</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link href="#experience">Experience</Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </React.Fragment>
-    );
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            expanded: false
+        };
+        
+        this.toTop = this.toTop.bind(this);
+        this.toggleMobileNavState = this.toggleMobileNavState.bind(this);
+        this.closeMobileNav = this.closeMobileNav.bind(this);
+    }
+
+    toTop(){
+        scroll.scrollToTop({duration: 500});
+        this.closeMobileNav();
+    };
+
+    toggleMobileNavState = (expanded) => {
+        this.setState({expanded});
+    }
+
+    closeMobileNav = () => {
+        this.setState({expanded: false});
+    };
+
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar bg="transparent" expand="lg" fixed="top" expand="sm" onToggle={this.toggleMobileNavState} expanded={this.state.expanded} role="navigation" className="container-lg">
+                    <Navbar.Brand onClick={this.toTop}>
+                        <span className="text-bento-primary font-primary border border-3 border-bento-primary p-1 ml-2">
+                            bdhk
+                        </span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="menu" />
+                    <Navbar.Collapse id="menu" className="justify-content-end">
+                        <Nav className="font-secondary">
+                            <Nav.Item >
+                                <Link
+                                    activeClass="active"
+                                    to="about"
+                                    spy={true}
+                                    smooth={true}
+                                    duration={500}
+                                    offset={-57}
+                                    className="nav-link"
+                                    onClick={this.closeMobileNav}
+                                    >
+                                    About
+                                </Link>
+                            </Nav.Item>
+                            <Nav.Item >
+                                <Link
+                                    activeClass="active"
+                                    to="experience"
+                                    spy={true}
+                                    smooth={true}
+                                    duration={500}
+                                    offset={-57}
+                                    className="nav-link"
+                                    onClick={this.closeMobileNav}
+                                    >
+                                    Experience
+                                </Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </React.Fragment>
+        );
+    }
+    
 }
 
 export default NavbarMain;
