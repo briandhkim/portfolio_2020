a6a2cd7f83d4a27a4561cfd662da051425ff3331
bkim <w1057216@g.ucla.edu>
2020-11-27 19:29:28 -0800

    Finish skills section Initial version of list style skills section finished

--- src/components/skills/text_view.js
+++ src/components/skills/text_view.js
@@ -167,6 +167,19 @@ const TextView = (props) => {
                                 Git
                             </ListGroup.Item>
                             <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
+                                <svg className="skills-list-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 2 30 30" fill="none">
+                                    <path d="M4.77044 5C4.29089 5 3.93123 5.43959 4.01115 5.87918L7.24814 25.7007C7.32806 26.2203 7.76766 26.5799 8.28717 26.5799H23.9526C24.3123 26.5799 24.632 26.3002 24.7119 25.9405L27.9888 5.91915C28.0688 5.43959 27.7091 5.03997 27.2296 5.03997L4.77044 5ZM18.5177 19.3067H13.5223L12.2035 12.2333H19.7565L18.5177 19.3067Z" fill="#2684FF" />
+                                    <path d="M26.9098 12.2333H19.7165L18.5176 19.3067H13.5222L7.64771 26.3002C7.64771 26.3002 7.92744 26.54 8.32707 26.54H23.9925C24.3522 26.54 24.6719 26.2602 24.7518 25.9005L26.9098 12.2333Z" fill="url(#paint0_linear)" />
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="28.5925" y1="14.2265" x2="16.672" y2="23.5316" gradientUnits="userSpaceOnUse">
+                                            <stop offset="0.176" stopColor="#0052CC" />
+                                            <stop offset="1" stopColor="#2684FF" />
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                                Bitbucket
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
                                 <svg viewBox="0 0 128 128" className="skills-list-icon">
                                     <circle fill="#fff" cx="64.149" cy="64.236" r="60.999"></circle><path fillOpacity=".1" d="M102.966 75.327c0-21.439-17.379-38.819-38.817-38.819s-38.818 17.38-38.818 38.819h11.09c0-15.314 12.415-27.727 27.727-27.727 15.313 0 27.727 12.413 27.727 27.727"></path><circle fillOpacity=".1" cx="66.922" cy="71.999" r="21.072"></circle><linearGradient id="a" gradientUnits="userSpaceOnUse" x1="395.191" y1="484.168" x2="395.191" y2="484.723" gradientTransform="matrix(82 0 0 82 -32341.5 -39660.313)"><stop offset="0" stopColor="#81B4E0"></stop><stop offset="1" stopColor="#0C5A94"></stop></linearGradient><circle fill="url(#a)" cx="64.149" cy="64.235" r="22.736"></circle><linearGradient id="b" gradientUnits="userSpaceOnUse" x1="-608.91" y1="-597.648" x2="-608.91" y2="-547.185" gradientTransform="translate(675 599.775)"><stop offset="0" stopColor="#F06B59"></stop><stop offset="1" stopColor="#DF2227"></stop></linearGradient><path fill="url(#b)" d="M119.602 36.508c-15.266-30.716-52.542-43.24-83.259-27.974-9.578 4.761-17.764 11.913-23.765 20.766l24.955 43.253c-4.597-14.606 3.521-30.174 18.127-34.77 2.567-.808 5.243-1.238 7.935-1.274"></path><linearGradient id="c" gradientUnits="userSpaceOnUse" x1="-657.835" y1="-491.393" x2="-632.327" y2="-533.537" gradientTransform="translate(675 599.775)"><stop offset="0" stopColor="#388B41"></stop><stop offset="1" stopColor="#4CB749"></stop></linearGradient><path fill="url(#c)" d="M12.578 29.3c-19.1 28.492-11.486 67.071 17.005 86.171 8.814 5.909 18.997 9.461 29.575 10.319l26.063-44.363c-9.745 11.811-27.22 13.486-39.032 3.74-4.011-3.309-7.012-7.679-8.657-12.613"></path><linearGradient id="d" gradientUnits="userSpaceOnUse" x1="-572.385" y1="-486.91" x2="-599.557" y2="-552.345" gradientTransform="translate(675 599.775)"><stop offset="0" stopColor="#E4B022"></stop><stop offset=".3" stopColor="#FCD209"></stop></linearGradient><path fill="url(#d)" d="M59.158 125.791c34.204 2.585 64.027-23.047 66.613-57.25.834-11.037-1.295-22.093-6.17-32.031h-56.006c15.312.07 27.67 12.541 27.598 27.854-.028 6.195-2.131 12.204-5.972 17.064"></path><linearGradient id="e" gradientUnits="userSpaceOnUse" x1="-649.391" y1="-528.885" x2="-649.391" y2="-573.247" gradientTransform="translate(675 599.775)"><stop offset="0" stopOpacity=".15"></stop><stop offset=".3" stopOpacity=".06"></stop><stop offset="1" stopOpacity=".03"></stop></linearGradient><path fill="url(#e)" d="M12.578 29.3l24.955 43.253c-1.849-6.221-1.457-12.893 1.107-18.854l-24.954-26.063"></path><linearGradient id="f" gradientUnits="userSpaceOnUse" x1="-588.158" y1="-514.559" x2="-618.657" y2="-483.505" gradientTransform="translate(675 599.775)"><stop offset="0" stopOpacity=".15"></stop><stop offset=".3" stopOpacity=".06"></stop><stop offset="1" stopOpacity=".03"></stop></linearGradient><path fill="url(#f)" d="M59.158 125.791l26.063-44.363c-4.112 4.904-9.794 8.233-16.082 9.426l-11.091 34.937"></path><linearGradient id="g" gradientUnits="userSpaceOnUse" x1="-588.6" y1="-505.621" x2="-584.163" y2="-549.431" gradientTransform="translate(675 599.775)"><stop offset="0" stopOpacity=".15"></stop><stop offset=".3" stopOpacity=".06"></stop><stop offset="1" stopOpacity=".03"></stop></linearGradient><path fill="url(#g)" d="M119.602 36.508h-56.007c8.436.039 16.396 3.918 21.626 10.537l35.491-8.873"></path>
                                 </svg>
@@ -264,19 +277,6 @@ const TextView = (props) => {
                         <ListGroup variant="flush" as="ul" className="font-secondary">
                             <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
                                 <svg className="skills-list-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 2 30 30" fill="none">
-                                    <path d="M4.77044 5C4.29089 5 3.93123 5.43959 4.01115 5.87918L7.24814 25.7007C7.32806 26.2203 7.76766 26.5799 8.28717 26.5799H23.9526C24.3123 26.5799 24.632 26.3002 24.7119 25.9405L27.9888 5.91915C28.0688 5.43959 27.7091 5.03997 27.2296 5.03997L4.77044 5ZM18.5177 19.3067H13.5223L12.2035 12.2333H19.7565L18.5177 19.3067Z" fill="#2684FF" />
-                                    <path d="M26.9098 12.2333H19.7165L18.5176 19.3067H13.5222L7.64771 26.3002C7.64771 26.3002 7.92744 26.54 8.32707 26.54H23.9925C24.3522 26.54 24.6719 26.2602 24.7518 25.9005L26.9098 12.2333Z" fill="url(#paint0_linear)" />
-                                    <defs>
-                                        <linearGradient id="paint0_linear" x1="28.5925" y1="14.2265" x2="16.672" y2="23.5316" gradientUnits="userSpaceOnUse">
-                                            <stop offset="0.176" stopColor="#0052CC" />
-                                            <stop offset="1" stopColor="#2684FF" />
-                                        </linearGradient>
-                                    </defs>
-                                </svg>
-                                Bitbucket
-                            </ListGroup.Item>
-                            <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
-                                <svg className="skills-list-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 2 30 30" fill="none">
                                     <path d="M27.0055 4H15.4512C15.4512 6.88 17.7826 9.21143 20.6626 9.21143H22.7883V11.2686C22.7883 14.1486 25.1197 16.48 27.9997 16.48V4.99429C27.9997 4.44571 27.554 4 27.0055 4Z" fill="#2684FF" />
                                     <path d="M21.2799 9.76001H9.72559C9.72559 12.64 12.057 14.9714 14.937 14.9714H17.0627V17.0286C17.0627 19.9086 19.3942 22.24 22.2742 22.24V10.7543C22.2742 10.2057 21.8284 9.76001 21.2799 9.76001Z" fill="url(#paint0_linear)" />
                                     <path d="M15.5543 15.52H4C4 18.4 6.33143 20.7314 9.21143 20.7314H11.3371V22.7886C11.3371 25.6686 13.6686 28 16.5486 28V16.5143C16.5486 15.9657 16.1029 15.52 15.5543 15.52Z" fill="url(#paint1_linear)" />
@@ -292,6 +292,25 @@ const TextView = (props) => {
                                     </defs>
                                 </svg>
                                 Jira
+                            </ListGroup.Item>
+                            <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
+                                <svg className="skills-list-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="2 2 30 30" fill="none">
+                                    <path d="M4.87273 21.5569C4.61902 21.9559 4.32905 22.4274 4.11158 22.7901C3.89411 23.1528 4.00285 23.6243 4.3653 23.8419L9.33088 26.8884C9.69333 27.106 10.1645 26.9972 10.382 26.6345C10.5632 26.3081 10.8169 25.8729 11.1069 25.4014C13.0641 22.1735 15.0576 22.5362 18.6096 24.2408L23.539 26.562C23.9377 26.7434 24.3726 26.562 24.5538 26.1993L26.9098 20.8678C27.091 20.5052 26.9098 20.0337 26.5473 19.8523C25.4962 19.3808 23.4302 18.4016 21.618 17.4949C14.9126 14.267 9.22215 14.4846 4.87273 21.5569Z" fill="url(#paint0_linear)" />
+                                    <path d="M27.1274 9.44314C27.3812 9.04419 27.6711 8.57269 27.8886 8.21001C28.1061 7.84732 27.9973 7.37583 27.6349 7.15822L22.6693 4.11165C22.3068 3.89404 21.8356 4.00285 21.6182 4.36553C21.437 4.69195 21.1832 5.12718 20.8933 5.59867C18.936 8.82657 16.9426 8.46389 13.3905 6.75926L8.46118 4.43807C8.06248 4.25673 7.62754 4.43807 7.44632 4.80076L5.09038 10.1322C4.90916 10.4949 5.09038 10.9664 5.45283 11.1478C6.50394 11.6193 8.56992 12.5985 10.3822 13.5052C17.0875 16.6969 22.778 16.5155 27.1274 9.44314Z" fill="url(#paint1_linear)" />
+                                    <defs>
+                                        <linearGradient id="paint0_linear" x1="26.7906" y1="28.4674" x2="11.7924" y2="19.8546" gradientUnits="userSpaceOnUse">
+                                            <stop stopColor="#0052CC" />
+                                            <stop offset="0.9182" stopColor="#2380FB" />
+                                            <stop offset="1" stopColor="#2684FF" />
+                                        </linearGradient>
+                                        <linearGradient id="paint1_linear" x1="5.20945" y1="2.52323" x2="20.2079" y2="11.1361" gradientUnits="userSpaceOnUse">
+                                            <stop stopColor="#0052CC" />
+                                            <stop offset="0.9182" stopColor="#2380FB" />
+                                            <stop offset="1" stopColor="#2684FF" />
+                                        </linearGradient>
+                                    </defs>
+                                </svg>
+                                Confluence
                             </ListGroup.Item>
                             <ListGroup.Item as="li" className="pb-0 bg-transparent border-0">
                                 <svg className="skills-list-icon" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="60 60 150 150" xmlSpace="preserve">
