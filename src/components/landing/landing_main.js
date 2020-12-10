57bbd42aed83f107c04e51be8ed8f7e40a37db7f
bkim <w1057216@g.ucla.edu>
2020-12-10 00:09:05 -0800

    Replace landing parallax

--- src/components/landing/landing_main.js
+++ src/components/landing/landing_main.js
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faAngleDown} from '@fortawesome/free-solid-svg-icons';
 import {animateScroll as scroll} from 'react-scroll';
+import {Parallax, Background} from 'react-parallax';
 
 const LandingMain = (props) => {
 
@@ -16,6 +17,23 @@ const LandingMain = (props) => {
 
     return (
         <React.Fragment>
+            <div className="position-absolute">
+                <Parallax
+                    bgImage={require('../../assets/landing_bg.png')}
+                    strength={300}
+                    style={{
+                        width: '90vw',
+                        height: '100vh',
+                        left: 'calc(-5.55% + 10vw)'
+                    }}
+                    bgImageStyle={{
+                        top:'5%',
+                        height: '85%',
+                        width: 'auto'
+                    }}
+                >
+                </Parallax>
+            </div>
             <Container fluid id="landing" className="d-flex justify-content-center align-items-end h-100 bg-plain-light">
                 <div className="landing-start-btn button-salmon" onClick={toAbout}>
                     {/* <FontAwesomeIcon icon={faMouse} size="lg" fixedWidth className="d-block"/> */}
